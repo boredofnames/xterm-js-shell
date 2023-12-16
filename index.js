f893d4c910fa21652d5e5ac05e75e72031a6e6a4
Anthony K. <kilospade@gmail.com>
2023-12-16 01:27:25 -0600

    prettier format

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import LocalEchoController from 'local-echo'
-import {EventIterator} from 'event-iterator'
+import { EventIterator } from 'event-iterator'
 
 const style = require('ansi-styles')
 const minimist = require('minimist')
 const stringToArgv = require('string-to-argv')
 
-const ERROR_NOT_FOUND = (command) => `Command Not Found: ${command}`
-const ERROR_ALREADY_REGISTERED = (command) => `Command Already Registered: ${command}`
+const ERROR_NOT_FOUND = command => `Command Not Found: ${command}`
+const ERROR_ALREADY_REGISTERED = command =>
+	`Command Already Registered: ${command}`
 
 const WHITESPACE_REGEX = /[\s\r\n]+/g
 
@@ -18,252 +19,260 @@ const WHITESPACE_REGEX = /[\s\r\n]+/g
  */
 
 /**
-  * Autocomplete Callback structure
-  * @callback AutocompleteProvider
-  * @param {number} index The index in the args array to autocomplete
-  * @param {Array<string>} args The list of arguments being passed to the command
-  * @return {Array<string>} The list of options the user could try
-  */
+ * Autocomplete Callback structure
+ * @callback AutocompleteProvider
+ * @param {number} index The index in the args array to autocomplete
+ * @param {Array<string>} args The list of arguments being passed to the command
+ * @return {Array<string>} The list of options the user could try
+ */
 
 /** Shell abstraction for Xterm.js */
 export default class XtermJSShell {
-  /**
-   * Instantiate and attach a shell to the terminal
-   * @param {Terminal} term The xterm.js terminal
-   */
-  constructor (term) {
-    this.prompt = async () => '$ '
-    this.commands = new Map()
-    this.echo = new LocalEchoController(term)
-    this.term = term
-    this.env = {}
+	/**
+	 * Instantiate and attach a shell to the terminal
+	 * @param {Terminal} term The xterm.js terminal
+	 */
+	constructor(term) {
+		this.prompt = async () => '$ '
+		this.commands = new Map()
+		this.echo = new LocalEchoController(term)
+		this.term = term
+		this.env = {}
 
-    this.attached = true
+		this.attached = true
 
-    this.echo.addAutocompleteHandler(this.autoCompleteCommands.bind(this))
-  }
+		this.echo.addAutocompleteHandler(
+			this.autoCompleteCommands.bind(this),
+		)
+	}
 
-  /**
-   * Detach the shell from xtermjs
-   */
-  detach () {
-    if (!this.attached) return
-    this.echo.detach()
-    this.attached = false
-  }
+	/**
+	 * Detach the shell from xtermjs
+	 */
+	detach() {
+		if (!this.attached) return
+		this.echo.detach()
+		this.attached = false
+	}
 
-  /**
-   * Attach the shell to the terminal
-   */
-  attach () {
-    if (this.attached) return
-    this.echo.attach()
-    this.attached = true
-  }
+	/**
+	 * Attach the shell to the terminal
+	 */
+	attach() {
+		if (this.attached) return
+		this.echo.attach()
+		this.attached = true
+	}
 
-  /**
-   * Utility for doing colors
-   * @return {object} The foreground instance of [ansi-colors](https://github.com/chalk/ansi-styles)
-   */
-  get color () {
-    return style.color
-  }
+	/**
+	 * Utility for doing colors
+	 * @return {object} The foreground instance of [ansi-colors](https://github.com/chalk/ansi-styles)
+	 */
+	get color() {
+		return style.color
+	}
 
-  get bgColor () {
-    return style.bgColor
-  }
+	get bgColor() {
+		return style.bgColor
+	}
 
-  /**
-   * Read-eval-print-loop, run this to start the shell
-   * @return {Promise} Resolves after a pass of the loop finishes
-   */
-  async repl () {
-    // Read
-    const prompt = await this.prompt()
-    const line = await this.echo.read(prompt)
+	/**
+	 * Read-eval-print-loop, run this to start the shell
+	 * @return {Promise} Resolves after a pass of the loop finishes
+	 */
+	async repl() {
+		// Read
+		const prompt = await this.prompt()
+		const line = await this.echo.read(prompt)
 
-    const argv = stringToArgv(line)
+		const argv = stringToArgv(line)
 
-    const command = argv.shift()
-    const parsed = minimist(argv)
+		const command = argv.shift()
+		const parsed = minimist(argv)
 
-    const raw_args = parsed._
+		const raw_args = parsed._
 
-    try {
-      // Eval / Print
-      await this.run(command, raw_args, parsed)
-    } catch (e) {
-      console.error(e)
-      await this.echo.println(e.message)
-    }
+		try {
+			// Eval / Print
+			await this.run(command, raw_args, parsed)
+		} catch (e) {
+			console.error(e)
+			await this.echo.println(e.message)
+		}
 
-    // Loop
-    this.repl()
-  }
+		// Loop
+		this.repl()
+	}
 
-  /**
-   * Run a command in the shell
-   * @param  {string}         command The name of the command to run
-   * @param  {Array<string>}  args    The list of command arguments to run
-   * @return {Promise}                Resolves after the command has finished
-   */
-  async run (command, args, flags) {
-    if (!this.commands.has(command)) throw new TypeError(ERROR_NOT_FOUND(command))
+	/**
+	 * Run a command in the shell
+	 * @param  {string}         command The name of the command to run
+	 * @param  {Array<string>}  args    The list of command arguments to run
+	 * @return {Promise}                Resolves after the command has finished
+	 */
+	async run(command, args, flags) {
+		if (!this.commands.has(command))
+			throw new TypeError(ERROR_NOT_FOUND(command))
 
-    const { fn } = this.commands.get(command)
+		const { fn } = this.commands.get(command)
 
-    const shell = new SubShell(this)
+		const shell = new SubShell(this)
 
-    const result = fn(shell, args, flags)
+		const result = fn(shell, args, flags)
 
-    if (result.then) {
-      await result
-    } else if (result.next) {
-      for await (let data of result) {
-        shell.print(data)
-      }
-    }
+		if (result.then) {
+			await result
+		} else if (result.next) {
+			for await (let data of result) {
+				shell.print(data)
+			}
+		}
 
-    shell.destroy()
-  }
+		shell.destroy()
+	}
 
-  /**
-   * Add a command to the shell
-   * @param  {string}        command The name of the command
-   * @param  {Command}      fn      Async function that takes a shell / args
-   * @return {XtermJSShell}          Returns self for chaining
-   */
-  command (command, fn, autocomplete) {
-    if (this.commands.has(command)) {
-      console.warn(ERROR_ALREADY_REGISTERED(command))
-    }
+	/**
+	 * Add a command to the shell
+	 * @param  {string}        command The name of the command
+	 * @param  {Command}      fn      Async function that takes a shell / args
+	 * @return {XtermJSShell}          Returns self for chaining
+	 */
+	command(command, fn, autocomplete) {
+		if (this.commands.has(command)) {
+			console.warn(ERROR_ALREADY_REGISTERED(command))
+		}
 
-    this.commands.set(command, {
-      command, fn, autocomplete
-    })
+		this.commands.set(command, {
+			command,
+			fn,
+			autocomplete,
+		})
 
-    return this
-  }
+		return this
+	}
 
-  // Internal command for auto completion of command names
-  autoCompleteCommands (index, tokens) {
-    const command = tokens[0]
-    if (index === 0) {
-      return [...this.commands.keys()]
-    } else if (this.commands.has(command)) {
-      const { autocomplete } = this.commands.get(command)
-      if (!autocomplete) return []
-      return autocomplete(index - 1, tokens.slice(1))
-    } else {
-      return []
-    }
-  }
+	// Internal command for auto completion of command names
+	autoCompleteCommands(index, tokens) {
+		const command = tokens[0]
+		if (index === 0) {
+			return [...this.commands.keys()]
+		} else if (this.commands.has(command)) {
+			const { autocomplete } = this.commands.get(command)
+			if (!autocomplete) return []
+			return autocomplete(index - 1, tokens.slice(1))
+		} else {
+			return []
+		}
+	}
 
-  async readChar (message) {
-    return this.echo.readChar(message)
-  }
+	async readChar(message) {
+		return this.echo.readChar(message)
+	}
 
-  async readLine (message) {
-    return this.echo.read(message)
-  }
+	async readLine(message) {
+		return this.echo.read(message)
+	}
 
-  async abortRead (reason) {
-    return this.echo.abortRead(reason)
-  }
+	async abortRead(reason) {
+		return this.echo.abortRead(reason)
+	}
 
-  async print (message) {
-    return this.term.write(message)
-  }
+	async print(message) {
+		return this.term.write(message)
+	}
 
-  async printLine (message) {
-    return this.echo.println(message)
-  }
+	async printLine(message) {
+		return this.echo.println(message)
+	}
 
-  async printList (list) {
-    return this.echo.printWide(list)
-  }
+	async printList(list) {
+		return this.echo.printWide(list)
+	}
 }
 
 class SubShell {
-  constructor (shell) {
-    this.shell = shell
-    this.destroyed = false
-  }
+	constructor(shell) {
+		this.shell = shell
+		this.destroyed = false
+	}
 
-  async * readStream () {
-    const iterator = new EventIterator((push) => {
-      this.shell.term.on('data', push)
-      this.shell.detach()
-    }, (push) => {
-      this.shell.term.off('data', push)
-      this.shell.attach()
-    })
+	async *readStream() {
+		const iterator = new EventIterator(
+			push => {
+				this.shell.term.on('data', push)
+				this.shell.detach()
+			},
+			push => {
+				this.shell.term.off('data', push)
+				this.shell.attach()
+			},
+		)
 
-    for await (let data of iterator) {
-      if (this.destroyed) break
-      yield data
-    }
-  }
+		for await (let data of iterator) {
+			if (this.destroyed) break
+			yield data
+		}
+	}
 
-  async readChar (message) {
-    this.checkDestroyed()
-    return this.shell.readChar(message)
-  }
+	async readChar(message) {
+		this.checkDestroyed()
+		return this.shell.readChar(message)
+	}
 
-  async readLine (message) {
-    this.checkDestroyed()
-    return this.shell.readLine(message)
-  }
+	async readLine(message) {
+		this.checkDestroyed()
+		return this.shell.readLine(message)
+	}
 
-  async abortRead (reason) {
-    this.checkDestroyed()
-    return this.shell.abortRead(reason)
-  }
+	async abortRead(reason) {
+		this.checkDestroyed()
+		return this.shell.abortRead(reason)
+	}
 
-  async print (message) {
-    this.checkDestroyed()
-    this.shell.print(message)
-  }
+	async print(message) {
+		this.checkDestroyed()
+		this.shell.print(message)
+	}
 
-  async printLine (message) {
-    this.checkDestroyed()
-    this.shell.printLine(message)
-  }
+	async printLine(message) {
+		this.checkDestroyed()
+		this.shell.printLine(message)
+	}
 
-  async printList (list) {
-    this.checkDestroyed()
-    this.shell.printList(list)
-  }
+	async printList(list) {
+		this.checkDestroyed()
+		this.shell.printList(list)
+	}
 
-  get color () {
-    return style.color
-  }
+	get color() {
+		return style.color
+	}
 
-  get bgColor () {
-    return style.bgColor
-  }
+	get bgColor() {
+		return style.bgColor
+	}
 
-  get commands () {
-    return [...this.shell.commands.keys()]
-  }
+	get commands() {
+		return [...this.shell.commands.keys()]
+	}
 
-  get env () {
-    return this.shell.env
-  }
+	get env() {
+		return this.shell.env
+	}
 
-  get cols () {
-    return this.shell.cols
-  }
+	get cols() {
+		return this.shell.cols
+	}
 
-  get rows () {
-    return this.shell.rows
-  }
+	get rows() {
+		return this.shell.rows
+	}
 
-  checkDestroyed () {
-    if (this.destroyed) throw new Error('Terminal destroyed')
-  }
+	checkDestroyed() {
+		if (this.destroyed) throw new Error('Terminal destroyed')
+	}
 
-  destroy () {
-    this.destroyed = true
-  }
+	destroy() {
+		this.destroyed = true
+	}
 }
